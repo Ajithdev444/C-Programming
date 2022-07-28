da8993ff1fb8b47fc0674b7ea70e4820180b2a85
Ajithdev444 <60468028+Ajithdev444@users.noreply.github.com>
2022-07-28 12:42:01 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,subtraction;
+    printf("enter the numbers:\n");
+    scanf("%d%d",&a,&b);
+    subtraction=a-b;
+    printf("sum is %d\n",subtraction);
+}
