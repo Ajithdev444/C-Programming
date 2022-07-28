4c18fc876be0cedbaaf2128e64830f74eb4c32f7
Ajithdev444 <60468028+Ajithdev444@users.noreply.github.com>
2022-07-28 16:22:17 +0530

    Add files via upload

---
+++ square and cube.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void
+main ()
+{
+    float sq,cube,n;
+    printf("enter the value\t");
+    scanf("%f",&n);
+    sq=n*n;
+    cube=n*n*n;
+    printf("the square and cube is:%f\t%f",sq,cube);
+}
