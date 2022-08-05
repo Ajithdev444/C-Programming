4a8fca3293bef33f475c849d250baf02df37ee3e
shaischaudhry <105935254+shaischaudhry@users.noreply.github.com>
2022-08-05 20:35:09 +0500

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 void main()
 {
-    int a,b,subtraction;
+    int a, b, subtraction;
     printf("enter the numbers:\n");
     scanf("%d%d",&a,&b);
-    subtraction=a-b;
+    subtraction = a-b;
+    
     printf("sum is %d\n",subtraction);
+
 }
