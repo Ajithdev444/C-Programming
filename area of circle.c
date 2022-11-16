78132e9b96fa8c0eedb0db67c1179d7b7fecee34
AJITHDEV C <60468028+Ajithdev444@users.noreply.github.com>
2022-11-16 12:19:27 +0530

    Update area of circle.c

--- area of circle.c
+++ area of circle.c
@@ -3,7 +3,7 @@ void
 main ()
 {
   float area, r;
-  printf ("enter the radius\t");
+  printf ("enter the radius \t");
   scanf ("%f", &r);
   area = 3.14 * r * r;
   printf ("the area of circle is:%f", area);
